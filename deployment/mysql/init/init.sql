070466c16bb34b863bdb00527067be64ca35e661
Tahiti <510099049@qq.com>
2024-03-20 22:02:38 +0800

    perf:优化docker部署mysql配置

--- deployment/mysql/init/init.sql
+++ deployment/mysql/init/init.sql
@@ -1,3 +1,7 @@
 -- init.sql
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+-- 切换到mysql数据库
+use mysql;
+-- 设置用户访问的ip
+update user set Host='%' where User='root';
+-- 刷新配置
+flush privileges;
